4def800a3134404a9a64216a804082c1ac2cd006
Hannah Casper <hannah.casper@accolade.com>
2019-07-18 10:34:46 -0700

    hoping this works

---
+++ job_scripts/script_prod.py
@@ -0,0 +1,31 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['TempDir', 'JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+## @type: DataSource
+## @args: [database = "jira-glue", table_name = "", transformation_ctx = "datasource0"]
+## @return: datasource0
+## @inputs: []
+datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "jira-glue", table_name = "", redshift_tmp_dir = args["TempDir"], transformation_ctx = "datasource0")
+## @type: ApplyMapping
+## @args: [mapping = [], transformation_ctx = "applymapping1"]
+## @return: applymapping1
+## @inputs: [frame = datasource0]
+applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [], transformation_ctx = "applymapping1")
+## @type: DataSink
+## @args: [connection_type = "s3", connection_options = {"path": "s3://jira-glue-result-063473290800"}, format = "csv", transformation_ctx = "datasink2"]
+## @return: datasink2
+## @inputs: [frame = applymapping1]
+datasink2 = glueContext.write_dynamic_frame.from_options(frame = applymapping1, connection_type = "s3", connection_options = {"path": "s3://jira-glue-result-063473290800"}, format = "csv", transformation_ctx = "datasink2")
+job.commit()
